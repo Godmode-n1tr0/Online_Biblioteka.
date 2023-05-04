611efc72dbcf1eb20646db9650ec8bdb3010b307
Godmode-n1tr0 <profi.roma@list.ru>
2023-05-04 20:45:22 +0500

    Исправили ошибку с книгой теперь всё отлично.

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -17,8 +17,8 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book.detail', args=[str(self.id)])
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
 
 
 class Genre(models.Model):
